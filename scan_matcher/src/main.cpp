0eb33845ae32865da3087c6cfb7ca4c464b62835
f11 <eieioeiji0501@gmail.com>
2026-08-05 20:53:18 +0900

    [add] implement scan matcher component with configuration and build setup

--- scan_matcher/src/main.cpp
+++ scan_matcher/src/main.cpp
@@ -1,9 +1,9 @@
-#include "simple_slam/simple_slam_component.hpp"
+#include "scan_matcher/scan_matcher_component.hpp"
 #include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<simple_slam::SimpleSlam>());
+  rclcpp::spin(std::make_shared<simple_slam::ScanMatcher>());
   rclcpp::shutdown();
   return 0;
 }
